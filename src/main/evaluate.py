4e8d3f80001bb981472ce6c85e30d619ef2461e9
kamalhammi <hammi.kamal.mpsi@hotmail.com>
2023-02-13 14:44:24 +0000

    Update evaluate.py

--- src/main/evaluate.py
+++ src/main/evaluate.py
@@ -49,11 +49,12 @@ def evaluate(commit : bool, matiere : str, tp : str, student : str, retour : str
             print(error)    
             
     os.chdir(student_project_folder)
-
+    print(os.listdir())
     sp.run(gitPull, shell=True)
     os.chdir("../../../../")
     gitClone = "git clone " + depot + " " + student_project_folder
     sp.run(gitClone, shell=True)
+
     #svnUpdate = "svn update " + student_project_folder
     #sp.run(svnUpdate, shell=True)
     
