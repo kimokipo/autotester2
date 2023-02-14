147ff31da86f5fbdfbfc8e94479cff08303fd2a1
kimokipo <hammi.kamal.mpsi@hotmail.com>
2023-02-14 15:40:33 +0100

    add detection commits of students script mill.py

--- src/main/mill.py
+++ src/main/mill.py
@@ -7,6 +7,7 @@ from ProjectEnv import ProjectEnv
 import evaluate
 import utility
 import importlib
+import csv
 
 # Fichier stockant les paths utiles
 from variables import *
@@ -16,27 +17,55 @@ WAIT_TIME = 5 # Temps en secondes entre 2 boucles de la moulinette
 def mill(matiere : str, tp : str):
     
     # Recuperation de la liste des chemins vers les dossiers eleve
-    students = [f.path for f in os.scandir(paths[matiere]["repository_path"]) if f.is_dir()][1:]
-        
+    
+    students = []
+    students_info = '1sn-autotester.csv'
+    with open(students_info) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            students.append(row[3])
+    students = students[1:]
+
+    project_path = paths[matiere]["repository_path"]
+
     # Setup de la "liste" des derniere revision de modalite controlee
     last_control = {}
-    for stu_dir in students :
+    for student_name in students :
+        if student_name != 'khammi':
+            continue
 
-        tp_path = stu_dir + "/" + tp
+        tp_path = project_path + "/" + student_name + "/" + tp
+
+        list_directory = tp_path.split('/')
+        for i in range(0,len(list_directory)+1):
+            try:
+                os.mkdir('/'.join(list_directory[0:i]))
+            except OSError as error:
+                print(error)  
+
+        depot = "https://" + paths["username"] + ":" + paths["password"] + "@gitlab.com/" + paths["gitlabArbre"] + matiere + "/" + student_name + "/" + tp + ".git"
+        gitClone = "git clone " + depot + " " + tp_path
+        sp.run(gitClone, shell=True)
 
         # Recuperation de la derniere revision de modalites
-        svnUpdate = "svn update " + tp_path
-        subprocess.run(svnUpdate, shell=True)
+        #svnUpdate = "svn update " + tp_path
+        #subprocess.run(svnUpdate, shell=True)
         
-        svnInfoCommand = "svn info " + tp_path + "/modalites.txt"
-        procInfo = subprocess.run(svnInfoCommand, stdout=subprocess.PIPE, shell=True)
+        #svnInfoCommand = "svn info " + tp_path + "/modalites.txt"
+        #procInfo = subprocess.run(svnInfoCommand, stdout=subprocess.PIPE, shell=True)
+        #info = str(procInfo.stdout.decode(sys.stdout.encoding))
+
+        os.chdir(tp_path)
+        gitLog = "git log modalites.txt"
+        procInfo = subprocess.run(gitLog, stdout=subprocess.PIPE, shell=True)
         info = str(procInfo.stdout.decode(sys.stdout.encoding))
         splitInfo = info.split()
         try :
-            index = splitInfo.index("Rev:")
+            index = splitInfo.index("commit")
         except ValueError:
             continue
-        last_control[tp_path] = int(splitInfo[index + 1])
+        last_control[tp_path] = splitInfo[index + 1]
+        os.chdir("../../../../")
     
     # Boucle Principale
     update_student_counter = 0
@@ -50,23 +79,32 @@ def mill(matiere : str, tp : str):
             update_student_counter = 0
                 
         #recherche d'une demande de tests
-        for stu_dir in students :
-            
-            tp_path = stu_dir + "/" + tp
-            
-            # Recuperation de la derniere revision ou modalites a ete modifie
-            svnUpdate = "svn update " + tp_path
-            subprocess.run(svnUpdate, shell=True)
+        for student_name in students :
+            if student_name != 'khammi':
+                continue
+
+            tp_path = project_path + "/" + student_name + "/" + tp
+
+            depot = "https://" + paths["username"] + ":" + paths["password"] + "@gitlab.com/" + paths["gitlabArbre"] + matiere + "/" + student_name + "/" + tp + ".git"
         
-            svnInfoCommand = "svn info " + tp_path + "/modalites.txt"
-            proc = subprocess.run(svnInfoCommand, stdout=subprocess.PIPE, shell=True)
-            info = str(proc.stdout.decode(sys.stdout.encoding))
+            # Recuperation de la derniere revision ou modalites a ete modifie
+            #svnUpdate = "svn update " + tp_path
+            #subprocess.run(svnUpdate, shell=True)
+
+            os.chdir(tp_path)
+            gitpull = "git pull --no-edit " + depot + " main"
+            sp.run(gitpull, shell = True)
+
+            gitLog = "git log modalites.txt"
+            procInfo = subprocess.run(gitLog, stdout=subprocess.PIPE, shell=True)
+            info = str(procInfo.stdout.decode(sys.stdout.encoding))
             splitInfo = info.split()
             try :
-                index = splitInfo.index("Rev:")
+                index = splitInfo.index("commit")
             except ValueError:
                 continue
-            revision = int(splitInfo[index + 1])
+            revision = splitInfo[index + 1]
+            os.chdir("../../../../")
             
             # Initialisation si nouveau TP
             if last_control[tp_path] == None :
@@ -84,12 +122,12 @@ def mill(matiere : str, tp : str):
                 scenarios = utility.get_scenarios(tp_path + "/modalites.txt", fichier_config.SCENARIOS)
 
                 scenarios_name = [scenario.run.__name__ for scenario in scenarios]
-                                                        
-                evaluate.evaluate(True, matiere, stu_dir.split('/')[-1], tp, *scenarios_name)
+                                                     
+                evaluate.evaluate(True, matiere, tp, student_name, "retour",  *scenarios_name)
 
                 # Renvoi des sources avec les divers retours
-                svnCommit = "svn commit -m \" Retour du test automatique \" " + tp_path + "/retour.txt"
-                subprocess.run(svnCommit, shell=True)
+                #svnCommit = "svn commit -m \" Retour du test automatique \" " + tp_path + "/retour.txt"
+                #subprocess.run(svnCommit, shell=True)
                 
                 # Maj de la revision
                 last_control[tp_path] = revision
